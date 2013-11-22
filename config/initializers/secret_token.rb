767102df7015ad496ee665ed3546d8091a07dc9e
gainsoft <v-hshabb@.redmond.corp.microsoft.com>
2013-11-21 21:50:25 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'e7b4021114413793b7736216207d59309dfa05482dd78cd6c3317693af2557a1180bc6da54e18fe29273f3c3bf9e9082d7d1ab0121ad0ece6f03fb4b2dac03bd'
